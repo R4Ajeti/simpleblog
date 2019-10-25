f56087987643581bf49df29eea50c67fb3c29a81
Rinor Ajeti <r4ajeti@gmail.com>
2019-10-25 13:47:17 +0200

    LINTER CORRECTION: Throw rubocop!

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class PagesController < ApplicationController
-    def about
-        @title = "About Us";
-        @content = "This is the about page";
-    end
+  def about
+    @title = 'About Us'
+    @content = 'This is the about page'
+  end
 end
